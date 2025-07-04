797dd329efa42d30f5e86a80d441f6d0d1a27481
Rafael Pancione <rafaelpancione@gmail.com>
2025-07-04 14:01:23 -0300

    Fix App tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
 
 test('renders home page heading', async () => {
-  render(<App />);
+  render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
   const heading = await screen.findByText(/BOAS VINDAS À/i);
   expect(heading).toBeInTheDocument();
+});
 
 test('renders Home link from header', async () => {
-  render(<App />);
-  const homeLink = await screen.findByRole('link', { name: /home/i });
-  expect(homeLink).toBeInTheDocument();
-
+  render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
+  const homeLinks = await screen.findAllByRole('link', { name: /home/i });
+  expect(homeLinks.length).toBeGreaterThan(0);
 });
